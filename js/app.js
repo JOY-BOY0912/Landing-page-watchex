2b3392d04b8db99f1e701acd583d5baa93a56f7e
JOY-BOY0912 <138391597+JOY-BOY0912@users.noreply.github.com>
2023-08-22 23:18:37 +0530

    initilised succesfully

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('.watch-slider').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+        autoplaySpeed: 500,
+    });
+  });
+  
